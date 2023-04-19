f9365be0679ab5aa810e47105c02adcf75ef218e
wftrop <maxence.ducoulombier@student.junia.com>
2023-04-19 12:00:37 +0200

    Add files via upload

--- code+interface.py
+++ code+interface.py
@@ -1,3 +1,13 @@
+#Python program to create
+# a file explorer in Tkinter
+  
+# import all components
+# from the tkinter library
+from tkinter import *
+  
+# import filedialog module
+from tkinter import filedialog
+
 from gcodeparser import GcodeParser
 import math
 
@@ -93,7 +103,7 @@ def reducepoints(points,precision, distanceMinimale): # réduit les points du fi
 
 
 def gcodeToNyiryoCode(fichierGcode):
-    with open('fichierGcode', 'r') as f:  
+    with open(fichierGcode, 'r') as f:  
       gcode = f.read()
     List , x_gcode , y_gcode , z_gcode , f_gcode , e_gcode=[],[],[],[],[],[]
     Gcode=GcodeParser(gcode).lines
@@ -125,3 +135,61 @@ def gcodeToNyiryoCode(fichierGcode):
     z=[i[2] for i in nouveauxPoints]
     f=[i[3] for i in nouveauxPoints]
     e=[i[4] for i in nouveauxPoints]
+
+    
+
+
+# Function for opening the
+# file explorer window
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("Text files",
+                                                        "*.gcode*"),
+                                                       ("all files",
+                                                        "*.*")))
+      
+    # Change label contents
+    label_file_explorer.configure(text="File Opened: "+filename)
+    gcodeToNyiryoCode(filename)
+                                                                                                  
+# Create the root window
+window = Tk()
+  
+# Set window title
+window.title('File Explorer')
+  
+# Set window size
+window.geometry("500x500")
+  
+#Set window background color
+window.config(background = "black")
+  
+# Create a File Explorer label
+label_file_explorer = Label(window,
+                            text = "selectionner votre fichier",
+                            width = 100, height = 4,
+                            fg = "blue")
+  
+      
+button_explore = Button(window,
+                        text = "Browse Files",
+                        command = browseFiles)
+  
+button_exit = Button(window,
+                     text = "Exit",
+                     command = exit)
+
+  
+# Grid method is chosen for placing
+# the widgets at respective positions
+# in a table like structure by
+# specifying rows and columns
+label_file_explorer.grid(column = 1, row = 1)
+  
+button_explore.grid(column = 1, row = 2)
+  
+button_exit.grid(column = 1,row = 3)
+  
+# Let the window wait for any events
+window.mainloop()
